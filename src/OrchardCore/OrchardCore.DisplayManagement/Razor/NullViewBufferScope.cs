55014cd0a3bff6a562f2af90b4bd43893d7c74c5
Sébastien Ros <sebastienros@gmail.com>
2020-01-02 18:18:15 -0800

    Fixing disposal

--- src/OrchardCore/OrchardCore.DisplayManagement/Razor/NullViewBufferScope.cs
+++ src/OrchardCore/OrchardCore.DisplayManagement/Razor/NullViewBufferScope.cs
@@ -1,6 +1,6 @@
-using System;
 using System.Buffers;
 using System.IO;
+using System.Text;
 using Microsoft.AspNetCore.Mvc.ViewFeatures.Buffers;
 
 namespace OrchardCore.DisplayManagement.Razor
@@ -11,7 +11,7 @@ namespace OrchardCore.DisplayManagement.Razor
 
         public TextWriter CreateWriter(TextWriter writer)
         {
-            return writer;
+            return new PassThroughWriter(writer);
         }
 
         public ViewBufferValue[] GetPage(int pageSize)
@@ -22,6 +22,38 @@ namespace OrchardCore.DisplayManagement.Razor
         public void ReturnSegment(ViewBufferValue[] segment)
         {
             _segments.Return(segment, true);
+        }
+    }
+
+    internal class PassThroughWriter : TextWriter
+    {
+        private readonly TextWriter _writer;
+
+        public PassThroughWriter(TextWriter writer)
+        {
+            _writer = writer;
+        }
+
+        public override Encoding Encoding => _writer.Encoding;
+
+        public override void Write(char value)
+        {
+            _writer.Write(value);
+        }
+
+        public override void Write(char[] buffer, int index, int count)
+        {
+            _writer.Write(buffer, index, count);
+        }
+
+        public override void Write(string value)
+        {
+            _writer.Write(value);
+        }
+
+        public override void Write(object value)
+        {
+            _writer.Write(value);
         }
     }
 }
